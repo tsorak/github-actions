88edc4459b3063bd64c28a5982ec62cc413cf484
tsorak <karost0249@gmail.com>
2023-03-28 10:39:59 +0200

    Add main function

--- main_test.ts
+++ main_test.ts
@@ -1,6 +1,9 @@
 import { assertEquals } from "https://deno.land/std@0.180.0/testing/asserts.ts";
-import { add } from "./main.ts";
+import { main } from "./main.ts";
 
-Deno.test(function addTest() {
-  assertEquals(add(2, 3), 5);
+Deno.test(function mainTest() {
+	const result = main();
+	console.log("Running main:", result);
+
+	assertEquals(result, "Hello World!");
 });
